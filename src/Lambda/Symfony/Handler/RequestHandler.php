2079a4e22782c4187396b136eca651102e315e7d
Andrew Matthews <andrew@andymdesign.com.au>
2019-12-09 16:17:05 +1000

    Handle X-FORWARDED headers properly, and set session cookie if required

--- src/Lambda/Symfony/Handler/RequestHandler.php
+++ src/Lambda/Symfony/Handler/RequestHandler.php
@@ -3,6 +3,7 @@
 namespace TopicAdvisor\Lambda\Symfony\Handler;
 
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -10,6 +11,7 @@ use TopicAdvisor\Lambda\RuntimeApi\Http\HttpRequestInterface;
 use TopicAdvisor\Lambda\RuntimeApi\Http\HttpResponse;
 use TopicAdvisor\Lambda\RuntimeApi\InvocationRequestInterface;
 use TopicAdvisor\Lambda\RuntimeApi\InvocationResponseInterface;
+use TopicAdvisor\Lambda\RuntimeApi\Util\LambdaDumper;
 
 class RequestHandler extends AbstractHandler
 {
@@ -22,16 +24,6 @@ class RequestHandler extends AbstractHandler
     public function __construct(KernelInterface $kernel)
     {
         parent::__construct($kernel);
-
-        if ($trustedProxies = getenv('TRUSTED_PROXIES') ?? false) {
-            Request::setTrustedProxies(
-                explode(',', $trustedProxies),
-                Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
-            );
-        }
-        if ($trustedHosts = getenv('TRUSTED_HOSTS') ?? false) {
-            Request::setTrustedHosts(explode(',', $trustedHosts));
-        }
 
         $this->httpFoundationFactory = new HttpFoundationFactory();
     }
@@ -46,13 +38,26 @@ class RequestHandler extends AbstractHandler
     }
 
     /**
-     * @param InvocationRequestInterface $lambdaRequest
+     * @param InvocationRequestInterface|HttpRequestInterface $lambdaRequest
      * @return InvocationResponseInterface
      * @throws \Exception
      */
     public function handle(InvocationRequestInterface $lambdaRequest): InvocationResponseInterface
     {
-        $symfonyResponse = $this->kernel->handle($this->httpFoundationFactory->createRequest($lambdaRequest));
+        $serverParams = $lambdaRequest->getServerParams();
+        if ($trustedProxies = $serverParams['TRUSTED_PROXIES'] ?? false) {
+            Request::setTrustedProxies(
+                explode(',', $trustedProxies),
+                Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
+            );
+        }
+        if ($trustedHosts = $serverParams['TRUSTED_HOSTS'] ?? false) {
+            Request::setTrustedHosts(explode(',', $trustedHosts));
+        }
+
+        $request = $this->httpFoundationFactory->createRequest($lambdaRequest);
+        $request->server->set('LAMBDA_PAYLOAD', $lambdaRequest->getPayload());
+        $symfonyResponse = $this->kernel->handle($request);
         return $this->toLambdaResponse($lambdaRequest, $symfonyResponse);
     }
 
@@ -66,6 +71,27 @@ class RequestHandler extends AbstractHandler
         $lambdaResponse = new HttpResponse($lambdaRequest->getInvocationId());
 
         $headers = $symfonyResponse->headers->all();
+        if ($cookies = $symfonyResponse->headers->getCookies()) {
+            $headers['Set-Cookie'] = $cookies;
+        }
+
+        // Set the session cookie if it was not supplied in the request (ie. session is being started in this response)
+        if (session_id() && !isset($lambdaRequest->getHeader('Cookie')[session_name()])) {
+            $cookie_options = $this->kernel->getContainer()->getParameter('session.storage.options');
+            $headers['Set-Cookie'][] = (string) new Cookie(
+                session_name(),
+                session_id(),
+                $cookie_options['cookie_lifetime'] ?? 0,
+                $cookie_options['cookie_path'] ?? '/',
+                $cookie_options['cookie_domain'] ?? '',
+                ($cookie_options['cookie_secure'] ?? 'auto') === 'auto'
+                    ? true : (bool) ($cookie_options['cookie_secure'] ?? 'auto'),
+                $cookie_options['cookie_httponly'] ?? true,
+                false,
+                $cookie_options['cookie_samesite'] ?? null
+            );
+        }
+
         $lambdaResponse->setStatusCode($symfonyResponse->getStatusCode());
         $lambdaResponse->setHeaders($headers);
         $lambdaResponse->setBody($symfonyResponse->getContent());
